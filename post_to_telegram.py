12cbf2d839f4240a9aec922e8206ae6cb04b09e3
Chop-Chopa <136185471+Chop-Chopa@users.noreply.github.com>
2025-04-24 18:39:01 +0300

    Добавил путь к папке

--- post_to_telegram.py
+++ post_to_telegram.py
@@ -21,12 +21,12 @@ def main():
     parser.add_argument('hours', type=int, default=int(os.environ["DELAY_HOURS"]), help="Задержка между публикациями в часах")
     args = parser.parse_args()
 
-    image_files = [image for image in os.listdir()]
+    image_files = [image for image in os.listdir('images')]
     random.shuffle(image_files)
 
     while True:
         if not image_files:
-            image_files = [image for image in os.listdir()]
+            image_files = [image for image in os.listdir('images')]
             random.shuffle(image_files)
 
         photo_filename = image_files.pop(0)
