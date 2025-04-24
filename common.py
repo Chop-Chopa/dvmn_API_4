20ef6feeba189dc7ed3362108f5b318457c80f74
Chop-Chopa <136185471+Chop-Chopa@users.noreply.github.com>
2025-04-24 17:13:21 +0300

    publish photo to Telegram channel.

--- common.py
+++ common.py
@@ -2,6 +2,7 @@ from urllib.parse import urlparse
 import os
 import requests
 import telegram
+import random
 
 
 def get_file_extension_from_url(url):
@@ -19,3 +20,9 @@ def save_image(url, file_path):
 def message_telegram(chat_id, token):
     bot = telegram.Bot(token=token)
     bot.send_message(chat_id=chat_id, text="I'm sorry Dave I'm afraid I can't do that.")
+
+
+def photo_telegram(chat_id, token):
+    bot = telegram.Bot(token=token)
+    filename = random.choice(os.listdir('images'))
+    bot.send_document(chat_id=chat_id, document=open(f'images/{filename}', 'rb'))
