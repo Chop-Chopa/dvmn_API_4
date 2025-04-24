cc72fe4bc1745c6bd554cd00274a60b89797d4f3
Chop-Chopa <136185471+Chop-Chopa@users.noreply.github.com>
2025-04-24 17:11:49 +0300

    publish text to Telegram channel.

--- common.py
+++ common.py
@@ -1,13 +1,21 @@
 from urllib.parse import urlparse
 import os
 import requests
+import telegram
+
 
 def get_file_extension_from_url(url):
     path = urlparse(url).path
     return os.path.splitext(path)[1]
+
 
 def save_image(url, file_path):
     response = requests.get(url)
     response.raise_for_status()
     with open(file_path, 'wb') as file:
         file.write(response.content)
+
+
+def message_telegram(chat_id, token):
+    bot = telegram.Bot(token=token)
+    bot.send_message(chat_id=chat_id, text="I'm sorry Dave I'm afraid I can't do that.")
