1a9905b5481aad9fe539393687ee5c60c567abe0
Chop-Chopa <136185471+Chop-Chopa@users.noreply.github.com>
2025-04-26 15:51:22 +0300

    Update common.py

--- common.py
+++ common.py
@@ -1,8 +1,6 @@
 from urllib.parse import urlparse
 import os
 import requests
-import telegram
-import random
 
 
 def get_file_extension_from_url(url):
@@ -17,12 +15,3 @@ def save_image(url, file_path):
         file.write(response.content)
 
 
-def message_telegram(chat_id, token):
-    bot = telegram.Bot(token=token)
-    bot.send_message(chat_id=chat_id, text="I'm sorry Dave I'm afraid I can't do that.")
-
-
-def photo_telegram(chat_id, token):
-    bot = telegram.Bot(token=token)
-    filename = random.choice(os.listdir('images'))
-    bot.send_document(chat_id=chat_id, document=open(f'images/{filename}', 'rb'))
