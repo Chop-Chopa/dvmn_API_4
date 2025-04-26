569f4a3b983b570e68d6ddd74a2217f329ca2b25
Chop-Chopa <136185471+Chop-Chopa@users.noreply.github.com>
2025-04-26 15:49:39 +0300

    Исправил код

--- fetch_nasa_apod.py
+++ fetch_nasa_apod.py
@@ -7,21 +7,24 @@ import requests
 import os.path
 
 
-def fetch_nasa_image(token, directory):
+API_IMAGES_LIMIT = 40
+
+
+def fetch_nasa_images(token, directory):
     payload = {
-        "count": "40",
+        "count": str(API_IMAGES_LIMIT),
         "api_key": token
     }
     response = requests.get("https://api.nasa.gov/planetary/apod", params=payload)
     response.raise_for_status()
-    image_response = response.json()
-    images_link = []
-    for image in range(len(image_response)):
-        path = urlparse(image_response[image]['url']).path
-        if os.path.splitext(path)[1] == '.jpeg' or os.path.splitext(path)[1] == '.jpg':
-            images_link.append(image_response[image]['url'])
+    images_metadata = response.json()
+    image_urls = []
+    for image_metadata in images_metadata:
+        path = urlparse(image_metadata['url']).path
+        if os.path.splitext(path)[1] in ('.jpeg', '.jpg'):
+            image_urls.append(image_metadata['url'])
 
-    for index, img_url in enumerate(images_link):
+    for index, img_url in enumerate(image_urls):
         filename = f'nasa_apod_{index}.jpg'
         save_image(img_url, directory / filename)
 
@@ -29,12 +32,12 @@ def fetch_nasa_image(token, directory):
 def main():
     load_dotenv()
 
-    nasa_acess_token = os.environ["NASA_TOKEN"]
+    nasa_access_token = os.environ["NASA_TOKEN"]
 
     output_dir = Path('images')
     output_dir.mkdir(exist_ok=True)
 
-    fetch_nasa_image(nasa_acess_token, output_dir)
+    fetch_nasa_images(nasa_access_token, output_dir)
 
 
 if __name__ == '__main__':
