faa7554f4c02354189990a9c15c6e9fc5f8024de
Chop-Chopa <136185471+Chop-Chopa@users.noreply.github.com>
2025-04-28 17:09:55 +0300

    Update fetch_nasa_apod.py

--- fetch_nasa_apod.py
+++ fetch_nasa_apod.py
@@ -1,43 +1,42 @@
-from urllib.parse import urlparse
-from common import save_image
+from common import save_image, get_file_extension_from_url
 from dotenv import load_dotenv
 from pathlib import Path
 import os
 import requests
-import os.path
 
 
 API_IMAGES_LIMIT = 40
 
 
-def fetch_nasa_images(token, directory):
-    payload = {
+def fetch_nasa_images(api_key, output_directory):
+    params = {
         "count": str(API_IMAGES_LIMIT),
-        "api_key": token
+        "api_key": api_key
     }
-    response = requests.get("https://api.nasa.gov/planetary/apod", params=payload)
+    response = requests.get("https://api.nasa.gov/planetary/apod", params=params)
     response.raise_for_status()
-    images_metadata = response.json()
-    image_urls = []
-    for image_metadata in images_metadata:
-        path = urlparse(image_metadata['url']).path
-        if os.path.splitext(path)[1] in ('.jpeg', '.jpg'):
-            image_urls.append(image_metadata['url'])
+    images_links = response.json()
+    photo_urls = []
 
-    for index, img_url in enumerate(image_urls):
+    for image_info in images_links:
+        file_extension = get_file_extension_from_url(image_info['url'])
+        if file_extension in ('.jpeg', '.jpg'):
+            photo_urls.append(image_info['url'])
+
+    for index, photo_url in enumerate(photo_urls):
         filename = f'nasa_apod_{index}.jpg'
-        save_image(img_url, directory / filename)
+        save_image(photo_url, output_directory / filename)
 
 
 def main():
     load_dotenv()
 
-    nasa_access_token = os.environ["NASA_TOKEN"]
+    nasa_token = os.environ["NASA_TOKEN"]
 
-    output_dir = Path('images')
-    output_dir.mkdir(exist_ok=True)
+    images_dir = Path('images')
+    images_dir.mkdir(exist_ok=True)
 
-    fetch_nasa_images(nasa_access_token, output_dir)
+    fetch_nasa_images(nasa_token, images_dir)
 
 
 if __name__ == '__main__':
